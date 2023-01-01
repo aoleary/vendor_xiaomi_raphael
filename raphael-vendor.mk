6ad549dd58e593a82cdae8b29aa4e733b2c0c4e4
firebird11 <hbgassel@gmail.com>
2023-01-01 20:25:53 +0100

    raphael: Add Volley
    Signed-off-by: SageOfD6Path <mail2anirban95@gmail.com>

--- raphael-vendor.mk
+++ raphael-vendor.mk
@@ -1220,6 +1220,7 @@ PRODUCT_PACKAGES += \
     qcrilmsgtunnel \
     camerax-vendor-extensions \
     tcmclient \
+    volley \
     com.android.hotwordenrollment.common.util \
     com.qti.dpmframework \
     dpmapi \
